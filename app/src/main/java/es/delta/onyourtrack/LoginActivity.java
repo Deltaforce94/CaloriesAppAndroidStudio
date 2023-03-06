7a0680cd5e21a8db1c37bde4a0a6e32e01e06ad0
Juan Manuel <deltaforce94@hotmail.es>
2023-03-06 12:44:35 +0100

    Initial commit

---
+++ app/src/main/java/es/delta/onyourtrack/LoginActivity.java
@@ -0,0 +1,46 @@
+package es.delta.onyourtrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    protected TextView titleText;
+    protected EditText emailText;
+    protected EditText passwordText;
+    protected Button loginButton;
+
+    private String email;
+    private String password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        titleText = findViewById(R.id.TextView1_login);
+        emailText = findViewById(R.id.EmailText1_login);
+        passwordText = findViewById(R.id.PasswordText1_login);
+        loginButton = findViewById(R.id.button1_login);
+
+
+        Toast.makeText(this, "Iniciando...", Toast.LENGTH_SHORT).show();
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                email = emailText.getText().toString();
+                password = passwordText.getText().toString();
+
+            }
+        });
+
+    }
+}
